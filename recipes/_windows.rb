b18baa8cdc577233003643be739979572dbde0fe
JJ Asghar <jj@chef.io>
2018-02-06 17:14:42 -0600

    Converted to windowspackage
    Signed-off-by: JJ Asghar <jj@chef.io>

--- recipes/_windows.rb
+++ recipes/_windows.rb
@@ -11,15 +11,17 @@ remote_file "#{Chef::Config['file_cache_path']}/VMware-tools.exe" do
 end
 
 unless "#{node['vmware-tools']['windows_reboot']}" == false
-  execute 'install the application' do
-    command 'C:\\chef\\cache\\VMware-tools.exe /s /v"/qn REBOOT=R"'
+  windows_package 'VMware-tools' do
+    source "#{Chef::Config['file_cache_path']}/VMware-tools.exe"
+    success_code [0,1618,1641,3010]
+    options  '/s /v"/qn REBOOT=R"'
     creates 'C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe'
   end
-
 else
-  execute 'install the application' do
-    command 'C:\\chef\\cache\\VMware-tools.exe /s /v"/qn"'
+  windows_package 'VMware-tools' do
+    source "#{Chef::Config['file_cache_path']}/VMware-tools.exe"
+    success_code [0,1618,1641,3010]
+    options  '/s /v"/qn"'
     creates 'C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe'
   end
-
 end
