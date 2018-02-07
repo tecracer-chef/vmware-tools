5bebd78280107b4a0c86896abbf550349e758129
JJ Asghar <jj@chef.io>
2018-02-06 18:07:23 -0600

    Fixing the options
    Signed-off-by: JJ Asghar <jj@chef.io>

--- recipes/_windows.rb
+++ recipes/_windows.rb
@@ -13,15 +13,15 @@ end
 unless "#{node['vmware-tools']['windows_reboot']}" == false
   windows_package 'VMware-tools' do
     source "#{Chef::Config['file_cache_path']}/VMware-tools.exe"
-    success_code [0,1618,1641,3010]
+    returns [0,1618,1641,3010]
     options  '/s /v"/qn REBOOT=R"'
-    creates 'C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe'
+    installer_type :custom
   end
 else
   windows_package 'VMware-tools' do
     source "#{Chef::Config['file_cache_path']}/VMware-tools.exe"
-    success_code [0,1618,1641,3010]
+    returns [0,1618,1641,3010]
     options  '/s /v"/qn"'
-    creates 'C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe'
+    installer_type :custom
   end
 end
