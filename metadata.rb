8fa7b45a549afda70092cc07a5e2eb1e7ba7ace5
JJ Asghar <jj@chef.io>
2018-04-23 15:28:56 -0500

    v2.0.0
    Updating to 10.2.0-7253323
    
    Signed-off-by: JJ Asghar <jj@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jj@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures vmware-tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '2.0.0'
 
 depends        'apt'
 depends	       'yum'
